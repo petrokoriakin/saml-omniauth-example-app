91bf6a455c194e129e3f52198fae28e36c7cb5c3
vladislav-gorbenko <gorbenko.vladislav@gmail.com>
2013-11-08 14:58:33 +0200

    add new users information, and sort

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
+
   def index
     @users = User.all
+    @users = User.order(sort_column + " " + sort_direction)
   end
+ 
+ private
+  
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end
